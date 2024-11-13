3303465e6fb59e1eff1df510ba542d5bfd58ac9e
daf <daffa199naufal@gmail.com>
2024-11-13 16:13:25 +0700

    added create mood from flutter and logout from flutter

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 from django.urls import reverse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
@@ -8,7 +9,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import reverse
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.core import serializers
 from django.utils.html import strip_tags
 from main.forms import MoodEntryForm
@@ -40,6 +41,25 @@ def create_mood_entry(request):
 
     context = {'form': form}
     return render(request, "create_mood_entry.html", context)
+
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
 
 
 @csrf_exempt
